68f9378282bffd3dcc74b7608f12ec36e2541b02
Mick Veldhuis <mickveldhuis@hotmail.nl>
2021-06-30 20:59:33 +0200

    Change the hour angle input to MOCCA from degrees to hours

--- mocca.py
+++ mocca.py
@@ -8,10 +8,10 @@ parser = argparse.ArgumentParser(
         )
 
 parser.add_argument('--az', action='store', type=float, default=0.0, help='dome azimuth: 0 to 360 deg | default: 0 deg')
-parser.add_argument('--ha', action='store', type=float, default=0.0, help='telescope hour angle: -180 to 180 deg | default: 0 deg')
+parser.add_argument('--ha', action='store', type=float, default=0.0, help='telescope hour angle: 0 to 24 h | default: 0 h')
 parser.add_argument('--dec', action='store', type=float, default=0.0, help='telescope declination -90 to 90 deg | default: 0 deg')
 parser.add_argument('-a', '--aperture', action='store', type=str, default='telescope', help='select aperture: telescope, finder, guider | default: telescope')
-parser.add_argument('-r', '--rate', action='store', type=int, default=4, help='no. rays (for decent results > 3; preferably 10+) | default: 4')
+parser.add_argument('-r', '--rate', action='store', type=int, default=4, help='no. radial circles of rays (for decent results > 3; preferably 10+) | default: 4')
 parser.add_argument('-v', '--visualise', default=False, action='store_true')
 
 args = parser.parse_args()
@@ -21,17 +21,17 @@ if __name__ == '__main__':
 
     if args.aperture == 'telescope':
         telescope = TelescopeAperture(rate=args.rate)
-        blockage = telescope.obstruction(args.ha, args.dec, args.az, plot_result=args.visualise)
+        blockage = telescope.obstruction(args.ha*15, args.dec, args.az, plot_result=args.visualise)
 
     elif args.aperture == 'guider':
         guider = GuiderAperture(rate=args.rate)
-        blockage = guider.obstruction(args.ha, args.dec, args.az, plot_result=args.visualise)
+        blockage = guider.obstruction(args.ha*15, args.dec, args.az, plot_result=args.visualise)
 
     elif args.aperture == 'finder':
         finder = FinderAperture(rate=args.rate)
-        blockage = finder.obstruction(args.ha, args.dec, args.az, plot_result=args.visualise)
+        blockage = finder.obstruction(args.ha*15, args.dec, args.az, plot_result=args.visualise)
 
     if blockage is not None:
         print('Obstruction = {:.2%}'.format(blockage))
     else:
-        print('The % obstruction could not be computed!')
+        print('ERROR:The % obstruction could not be computed!')
